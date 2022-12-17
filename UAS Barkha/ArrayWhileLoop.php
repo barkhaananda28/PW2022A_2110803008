efe1976c8c45a3d5aaa87ba4e06e038c43851e57
barkhaananda <barkhaananda28@gmail.com>
2022-12-17 14:17:54 +0700

    saya telah menyelesaikan UAS

---
+++ UAS Barkha/ArrayWhileLoop.php
@@ -0,0 +1,12 @@
+<?php 
+    //M. Rizky Barkha Ananda (2110803008)
+    echo "Menampilkan Nilai Array Menggunakan While :"."<br>";
+    
+    $angka = array(76,56,79,88,74,97,39,48,57);
+
+    $i = 0;
+    while ($i <= count($angka)) {
+        echo $angka[$i]."<br>";
+        $i++;
+    }
+?>
